0105a54a92a083e610b77b8a2b775f7eb166f149
dulguuned <dulguuned41@gmail.com>
2015-03-11 15:07:43 +0800

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Enkhdulguun on 3/11/15.
+//  Copyright (c) 2015 Enkhdulguun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Enkhdulguun on 3/11/15.
+//  Copyright (c) 2015 Enkhdulguun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
